f2925341475e679d6ebb524b615e379ec90a818d
ShoaibIND <hello@shoaibdev.me>
2021-01-06 21:53:01 +0530

    nav blend mode fix

--- app.js
+++ app.js
@@ -15,17 +15,6 @@ let toggle2 = document.querySelector('.darkmode2');
 toggle.addEventListener('click', invert);
 toggle2.addEventListener('click', invert);
 
-let navbar = document.querySelector('.navbar');
-document.addEventListener('scroll', () => {
-  if (window.scrollY > 399) {
-    navbar.classList.add('shadowed');
-    toggle2.classList.remove('toggle-button-nav');
-  } else {
-    navbar.classList.remove('shadowed');
-    toggle2.classList.add('toggle-button-nav');
-  }
-});
-
 document.querySelector('.nav-open').addEventListener('click', () => {
   document.querySelector(".overlay").style.width = '100%';
   document.querySelector("html").style.overflow = 'hidden';
@@ -39,7 +28,35 @@ document.querySelector('.closebtn').addEventListener('click', () => {
 let mobileLinks = document.querySelectorAll('.navItem');
 for (mobile of mobileLinks) {
   mobile.addEventListener('click', () => {
-    document.querySelector(".overlay").style.width ='0%';
+    document.querySelector(".overlay").style.width = '0%';
     document.querySelector("html").style.overflow = 'auto';
   })
 }
+
+let project = document.querySelector('#projectSection');
+let about = document.querySelector('#aboutSection');
+let contact = document.querySelector('#contactSection');
+let navbar = document.querySelector('.navbar');
+document.addEventListener('scroll', () => {
+  if (window.scrollY > (project.offsetTop - 20) && window.scrollY < (about.offsetTop - 100)) {
+    navbar.classList.add('shadowed');
+    toggle2.classList.remove('toggle-button-nav');
+  } else if (window.scrollY < (project.offsetTop - 20)) {
+    navbar.classList.remove('shadowed');
+    toggle2.classList.add('toggle-button-nav');
+  } else if (window.scrollY > (about.offsetTop - 100)) {
+    navbar.classList.remove('shadowed');
+  }
+
+  if (window.scrollY > about.offsetTop && window.scrollY < (contact.offsetTop - 100)) {
+    navbar.classList.add('lighten');
+  } else if (window.scrollY < about.offsetTop) {
+    navbar.classList.remove('lighten');
+  } else if (window.scrollY > (contact.offsetTop - 100)) {
+    navbar.classList.remove('lighten');
+  }
+
+  if (window.scrollY > contact.offsetTop) {
+    navbar.classList.add('shadowed');
+  }
+});
