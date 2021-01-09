2cc4e080cbf99eb57d1bd5f2bdb87a0a6862d484
ShoaibIND <hello@shoaibdev.me>
2021-01-10 01:29:52 +0530

    stutter fix

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+// Invert colors
 const invert = () => {
   document.querySelector('html').classList.toggle('invert');
   let imgs = document.querySelectorAll('img');
@@ -10,43 +11,56 @@ const invert = () => {
   document.querySelector('.dark2').classList.toggle('toggle-button');
 }
 
+// Darkmode Heading
 let toggle = document.querySelector('.darkmode');
 let toggle2 = document.querySelector('.darkmode2');
 toggle.addEventListener('click', invert);
 toggle2.addEventListener('click', invert);
 
+// Darkmode NAvigation
 let project = document.querySelector('#projectSection');
 let about = document.querySelector('#aboutSection');
 let contact = document.querySelector('#contactSection');
 let navbar = document.querySelector('.navbar');
+let navLink = document.querySelectorAll('.nav__list-item');
+
+
 document.addEventListener('scroll', () => {
   if (window.scrollY > (project.offsetTop - 20) && window.scrollY < (about.offsetTop - 100)) {
+    navLink[0].classList.add('active-nav');
+    navLink[1].classList.remove('active-nav');
+    navLink[2].classList.remove('active-nav');
     navbar.id = 'shadowed';
-    toggle2.classList.remove('toggle-button-nav');
-  } else if (window.scrollY < (project.offsetTop - 20)) {
-    navbar.removeAttribute('id');
     toggle2.classList.add('toggle-button-nav');
+  } else if (window.scrollY < (project.offsetTop - 20)) {
+    navLink[0].classList.remove('active-nav');
+    navbar.removeAttribute('id');
+    toggle2.classList.remove('toggle-button-nav');
   } else if (window.scrollY > (about.offsetTop - 100)) {
+    navLink[0].classList.remove('active-nav');
+    navLink[1].classList.add('active-nav');
+    navLink[2].classList.remove('active-nav');
     navbar.removeAttribute('id');
   }
 
   if (window.scrollY > about.offsetTop && window.scrollY < (contact.offsetTop - 100)) {
     navbar.classList.add('lighten');
-    toggle2.classList.remove('toggle-button-nav');
   } else if (window.scrollY < about.offsetTop) {
     navbar.classList.remove('lighten');
   } else if (window.scrollY > (contact.offsetTop - 100)) {
+    navLink[0].classList.remove('active-nav');
+    navLink[1].classList.remove('active-nav');
+    navLink[2].classList.add('active-nav');
     navbar.classList.remove('lighten');
   }
 
   if (window.scrollY > contact.offsetTop) {
     navbar.id = 'shadowed';
-    toggle2.classList.remove('toggle-button-nav');
+    toggle2.classList.add('toggle-button-nav');
   }
 });
 
 // Menu Bar
-
 let body = document.querySelector('body');
 let menu = document.querySelector('.menu-icon');
 let menuItems = document.querySelectorAll('.nav__list-item');
@@ -57,8 +71,8 @@ menu.addEventListener('click', () => {
 
 for (items of menuItems) {
   items.addEventListener('click', () => {
-  body.classList.toggle('nav-active');
-})
+    body.classList.toggle('nav-active');
+  })
 }
 
 let year = new Date;
@@ -73,44 +87,44 @@ tl.from('.stagger2', {
     opacity: 0,
     y: -50,
     stagger: .3,
-    ease: 'Power4.easeOut',
+    ease: 'Power1.easeNone',
     duration: 2
   }, '=.5')
   .from('.stagger1', {
     opacity: 0,
     x: -50,
     stagger: .3,
-    ease: 'Power4.easeOut',
+    ease: 'Power1.easeNone',
     duration: 2
   }, '-=2')
   .from('.logoLeft', {
     opacity: 50,
     x: -20,
-    ease: 'Power0.easeNone',
+    ease: 'Power1.easeNone',
     duration: 0.5
   }, '-=3')
   .from('.logoRight', {
     opacity: 50,
     x: 20,
-    ease: Power0,
+    ease: 'Power1.easeNone',
     duration: 0.5
   }, '-=2.5')
   .from('.logoCenter', {
     opacity: 50,
     y: -25,
-    ease: Power0,
+    ease: 'Power1.easeNone',
     duration: 0.5
   }, '-=2')
   .from('.darkmode', {
     opacity: 0,
     y: -25,
-    ease: Power0,
+    ease: 'Power1.easeNone',
     duration: 0.5
   }, '-=2')
   .from('.arrow', {
     opacity: 0,
     y: -50,
-    ease: Power0,
+    ease: 'Power1.easeNone',
     duration: 0.5
   }, '-=1.6');
 
@@ -124,8 +138,8 @@ stagger3.forEach((stagger3) => {
     opacity: 0,
     y: -50,
     stagger: .3,
-    ease: 'Power4.easeOut',
-    duration: 2
+    ease: 'Power1.easeNone',
+    duration: 1
   });
 });
 
@@ -138,8 +152,8 @@ portfolioButton.forEach((portfolioButton) => {
     },
     opacity: 0,
     y: 50,
-    ease: 'Power4.easeOut',
-    duration: 1
+    ease: 'Power1.easeNone',
+    duration: .8
   });
 });
 
@@ -152,7 +166,7 @@ portfolioImg.forEach((portfolioImg) => {
     },
     opacity: 0,
     x: 100,
-    ease: 'Power4.easeOut',
+    ease: 'Power1.easeNone',
     duration: 1
   });
 });
@@ -166,7 +180,7 @@ ImgLeft.forEach((ImgLeft) => {
     },
     opacity: 0,
     x: -100,
-    ease: 'Power4.easeOut',
+    ease: 'Power1.easeNone',
     duration: 1
   });
 });
@@ -179,8 +193,8 @@ gsap.from('.aboutStagger', {
   opacity: 0,
   y: 100,
   stagger: .3,
-  ease: 'Power4.easeOut',
-  duration: 2
+  ease: 'Power1.easeNone',
+  duration: 1
 });
 
 gsap.from('.stagger5', {
@@ -189,8 +203,8 @@ gsap.from('.stagger5', {
     start: "top bottom"
   },
   opacity: 0,
-    y: -50,
-    stagger: .3,
-    ease: 'Power4.easeOut',
-    duration: 2
+  x: -50,
+  stagger: .3,
+  ease: 'Power1.easeNone',
+  duration: 1
 });
